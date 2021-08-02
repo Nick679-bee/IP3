c926d93cbbf8bd899b9e2492e2ea3a9c06d2ae52
Nick679-bee <mwauranicholas10@gmail.com>
2021-08-02 08:17:23 +0300

    added imdex.js

---
+++ js/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    $(".design").click(function() {
+      $("#design").toggle("slow");
+    });
+    $("#design").click(function() {
+      $(".design").toggle("slow");
+    });
+    $(".development").click(function() {
+      $("#development").toggle("slow");
+    });
+    $("#development").click(function() {
+      $(".development").toggle("slow");
+    });
+    $(".product").click(function() {
+      $("#product").toggle("slow");
+    });
+    $("#product").click(function() {
+      $(".product").toggle("slow");
+    });
+    $('#work1').mouseover(function() {
+      $('.write1').show();
+    }).mouseout(function() {
+      $('.write1').hide();
+    });
+    $("#work2").mouseenter(function() {
+      $(".write2").show();
+    }).mouseleave(function() {
+      $(".write2").hide();
+    });
+    $("#work3").mouseenter(function() {
+      $(".write3").show();
+    }).mouseleave(function() {
+      $(".write3").hide();
+    });
+    $("#work4").mouseenter(function() {
+      $(".write4").show();
+    }).mouseleave(function() {
+      $(".write4").hide();
+    });
+    $("#work5").mouseenter(function() {
+      $(".write5").show();
+    }).mouseleave(function() {
+      $(".write5").hide();
+    });
+    $(".#work6").mouseenter(function() {
+      $(".write6").show();
+    }).mouseleave(function() {
+      $(".write6").hide();
+    });
+    $("#work7").mouseenter(function() {
+      $(".write7").show();
+    }).mouseleave(function() {
+      $(".write7").hide();
+    });
+    $("#work8").mouseenter(function() {
+      $(".write8").show();
+    }).mouseleave(function() {
+      $(".write8").hide();
+    });
+  
+})
